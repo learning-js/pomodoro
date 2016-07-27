5bed36dd8ea1aa73c2c766deb2109deb8eeb271d
Adriana Molina <moladriana@gmail.com>
2016-07-27 17:26:17 +0200

    Add and substract numbers to break/work timer

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
+  var selectBreak = parseInt($(".break").text());
+  var selectWork = parseInt($(".work").text());
 
   $("#minus-break").click(function(){
-    console.log($(".break").text());
+    selectBreak -= 1;
+    $(".break").html(selectBreak);
+  })
+
+  $("#plus-break").click(function(){
+    selectBreak += 1;
+    $(".break").html(selectBreak);
+  })
+
+  $("#minus-work").click(function(){
+    selectWork -= 1;
+    $(".work").html(selectWork);
+  })
+
+  $("#plus-work").click(function(){
+    selectWork += 1;
+    $(".work").html(selectWork);
   })
 
 })
