c28b495cefd50b351a7d25e277258a1f5b270a98
Adriana Molina <moladriana@gmail.com>
2016-08-03 11:20:31 +0200

    New function that stops the countdown

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
   var selectBreak = parseInt($(".break").text());
   var selectWork = parseInt($(".work").text());
-  var time = selectWork * 60;
+  var time = 0;
+  var breakTime = 0;
+  var minutes = 0;
+  var seconds = 0;
+  var loop = false;
+
+  function breakFunc(){
+    $(".title-section").html("Break time");
+    breakTime = selectBreak * 60;
+      while (breakTime !== 0) {
+        breakTime -= 1;
+        minutes = Math.floor(breakTime / 60);
+        seconds = breakTime - (minutes * 60);
+        $(".countdown").html(minutes + ":" + seconds);
+        console.log(minutes + ":" + seconds);
+      }
+  }
 
   $("#minus-break").click(function(){
     if(selectBreak >1){
@@ -18,6 +34,7 @@ $(document).ready(function(){
   $("#minus-work").click(function(){
     if(selectWork >1){
       selectWork -= 1;
+      time = selectWork * 60;
       $(".work").html(selectWork);
       $(".countdown").html(selectWork);
     }
@@ -25,20 +42,34 @@ $(document).ready(function(){
 
   $("#plus-work").click(function(){
     selectWork += 1;
+    time = selectWork * 60;
     $(".work").html(selectWork);
     $(".countdown").html(selectWork);
   })
 
   $(".rounded-timer").click(function(){
-    var minutes = 0;
-    var seconds = 0;
-    while (time >= 0){2
-      time -= 1;
-      minutes = Math.floor(time / 60);
-      seconds = time - (minutes * 60);
-      $(".countdown").html(minutes + ":" + seconds);
-    }
+    if(loop == false){
+      console.log("hola");
+      loop = true;
+          var timer = setInterval(countdownFunc, 1000)
+      function countdownFunc (){
+          time -= 1;
+          minutes = Math.floor(time / 60);
+          seconds = time - (minutes * 60);
+          $(".countdown").html(minutes + ":" + seconds);
+          console.log(minutes + ":" + seconds);
 
+        if (time == 0){
+          breakFunc();
+        }
+
+      }
+    }
+    if(loop){
+      console.log("adios");
+      loop = false;
+      clearInterval(timer);
+    }
   })
 
 })
