639e38208b84d1a670d47a45cae48f380b1efcd0
Adriana Molina <moladriana@gmail.com>
2016-08-04 21:57:02 +0200

    Fixed functions start/stop countdown

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,75 +1,87 @@
 $(document).ready(function(){
   var selectBreak = parseInt($(".break").text());
   var selectWork = parseInt($(".work").text());
-  var time = 0;
-  var breakTime = 0;
-  var minutes = 0;
-  var seconds = 0;
+  var time;
   var loop = false;
+  var countdownStarted = false;
+  var countId;
 
-  function breakFunc(){
-    $(".title-section").html("Break time");
-    breakTime = selectBreak * 60;
-      while (breakTime !== 0) {
-        breakTime -= 1;
-        minutes = Math.floor(breakTime / 60);
-        seconds = breakTime - (minutes * 60);
-        $(".countdown").html(minutes + ":" + seconds);
-        console.log(minutes + ":" + seconds);
-      }
+  function countdownStart() {
+    countId = setInterval(updateCountdown, 1000);
+  }
+
+  function countdownStop() {
+    clearInterval(countId);
+  }
+
+  function updateCountdown() {
+    var min = Math.floor(time / 60);
+    var sec = time - (min * 60);
+    if (min < 10){
+      min = "0" + min;
+    }
+    if (sec < 10){
+      sec = "0" + sec;
+    }
+    var timerText = min + ":" + sec;
+    $(".countdown").html(timerText);
+    time -= 1;
   }
 
   $("#minus-break").click(function(){
-    if(selectBreak >1){
-      selectBreak -= 1;
-      $(".break").html(selectBreak);
+    if (!loop){
+      if(selectBreak >1){
+        selectBreak -= 1;
+        $(".break").html(selectBreak);
+      }
     }
   })
 
   $("#plus-break").click(function(){
-    selectBreak += 1;
-    $(".break").html(selectBreak);
+    if (!loop){
+      selectBreak += 1;
+      $(".break").html(selectBreak);
+    }
   })
 
   $("#minus-work").click(function(){
-    if(selectWork >1){
-      selectWork -= 1;
-      time = selectWork * 60;
-      $(".work").html(selectWork);
-      $(".countdown").html(selectWork);
+    if (!loop){
+      if(selectWork >1){
+        selectWork -= 1;
+        $(".work").html(selectWork);
+        $(".countdown").html(selectWork);
+        countdownStarted = false;
+      }
     }
   })
 
   $("#plus-work").click(function(){
-    selectWork += 1;
-    time = selectWork * 60;
-    $(".work").html(selectWork);
-    $(".countdown").html(selectWork);
+    if (!loop){
+      selectWork += 1;
+      $(".work").html(selectWork);
+      $(".countdown").html(selectWork);
+      countdownStarted = false;
+    }
   })
 
   $(".rounded-timer").click(function(){
-    if(loop == false){
-      console.log("hola");
-      loop = true;
-          var timer = setInterval(countdownFunc, 1000)
-      function countdownFunc (){
-          time -= 1;
-          minutes = Math.floor(time / 60);
-          seconds = time - (minutes * 60);
-          $(".countdown").html(minutes + ":" + seconds);
-          console.log(minutes + ":" + seconds);
-
-        if (time == 0){
-          breakFunc();
-        }
-
-      }
-    }
-    if(loop){
-      console.log("adios");
+    if (loop) {
+      console.log("debo parar");
+      countdownStop();
       loop = false;
-      clearInterval(timer);
+    }
+    else {
+      console.log("debo seguir");
+      if (!countdownStarted) {
+        // get time from html
+        time = parseInt($(".countdown").text()) * 60;
+        countdownStarted = true;
+        updateCountdown();
+      }
+      countdownStart();
+      loop = true;
     }
   })
+
 
 })
